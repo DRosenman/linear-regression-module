0d351128e600288d67f80b1cfe3088bc106edb8d
Dave Rosenman <rosenmd1@tcnj.edu>
2017-05-07 18:24:42 -0400

    Add files via upload

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def slope(x,y,through_origin = False):
     x = np.array(x)
@@ -41,21 +42,48 @@ def intercept_error(x,y,through_origin = False):
         D = ((x-x.mean())**2).sum()
         residuals_squared = (y - slope(x,y)*x - intercept(x,y))**2
         return np.sqrt(((1/n) + (x.mean()**2)/D)*residuals_squared.sum()/(n-2))
+    else:
+        return 'NaN'
 
-def plot_graph(x,y, through_origin = False):
+def summary_stats(x,y,through_origin = False):
+    return (slope(x,y,through_origin),intercept(x,y,through_origin),
+            slope_error(x,y,through_origin),intercept_error(x,y,through_origin),
+                       correlation_coefficient(x,y))
+
+
+ 
+def print_summary_stats(x,y,through_origin = False):
+    if through_origin == False:
+        print(pd.DataFrame([[slope(x,y),intercept(x,y),slope_error(x,y)]], 
+                            columns = ['Slope', 'Intercept','Std. Error, Slope'],
+                                       index = ['']))
+        print("")
+        print(pd.DataFrame([[intercept_error(x,y),correlation_coefficient(x,y)]],
+                            columns = ['Std. Error, Intercept', 'r'],
+                            index = ['']))
+    if through_origin == True:
+        print(pd.DataFrame([[slope(x,y,through_origin = True),intercept(x,y,through_origin = True),
+                            slope_error(x,y,through_origin = True)]], 
+                            columns = ['Slope', 'Intercept','Std. Error, Slope'],
+                            index = ['']))
+        print("")
+        
+        print(pd.DataFrame([[correlation_coefficient(x,y)]],columns = ['r'], index = ['']))
+
+def correlation_coefficient(x,y):
     x = np.array(x)
     y = np.array(y)
-    plt.plot(x, y, 'o', label='original data')
-    if through_origin == False:
-        plt.plot(x, intercept(x,y) + slope(x,y)*x, 'r', label='fitted curve')
-    else:
-        plt.plot(x,slope(x,y,True)*x, 'r', label = 'fitted line')
-def plot_y_over_x(x,y):
-    x = np.array(x)
-    y = np.array(y) 
-    plt.plot(x[x!=0],y[x!=0]/x[x!=0],'o',label = 'original data')
-    plt.hlines(slope(x,y,True), x.min(), x.max(), colors='k', linestyles='solid', label = 'best fit line')
+    n = len(x)
+    xy = x*y
+    return (n*(xy.sum()) - (x.sum())*(y.sum()))/((((n*(x**2).sum()) - (x.sum())**2)**.5)*((n*((y**2).sum()) - (y.sum())**2)**.5))
+
+
+def r(x,y):
+    return correlation_coefficient(x,y)
+
+def r_squared(x,y):
+    return (correlation_coefficient(x,y))**2
+
     
-        
-        
-        
+
+
